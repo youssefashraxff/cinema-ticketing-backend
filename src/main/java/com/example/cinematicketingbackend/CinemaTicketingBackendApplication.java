7c483349f7223a2207e1a0f2607cfd7ff2d28189
Youssef Ashraf <y.ashraf.ibrahim@gmail.com>
2025-12-05 20:44:29 +0200

    initial commit

---
+++ src/main/java/com/example/cinematicketingbackend/CinemaTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cinematicketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaTicketingBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cinematicketingbackend/CinemaTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinematicketingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketingBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
